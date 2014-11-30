254f84995bf1d78f64178332dcb4c7f2d674c6c6
Matt Welson <me_number3@hotmail.com>
2014-11-30 16:10:48 +1300

    adding fitbit controller

---
+++ GoogleCharts/Controllers/FitbitController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleCharts.Web.Controllers
+{
+    public class FitbitController : Controller
+    {
+        // GET: Fitbit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Auth()
+        {
+            return Content("Hello");
+        }
+
+        public ActionResult Callback()
+        {
+            return Content("Callback");
+        }
+    }
+}
